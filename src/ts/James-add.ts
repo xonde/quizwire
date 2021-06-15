6884ac685720bd4cdceb0b44bab60a6bc7f3f1be
James Reilly <james.reilly.780@gmail.com>
2021-06-15 09:45:06 +0100

    James-add

---
+++ src/ts/James-add.ts
@@ -0,0 +1,14 @@
+function initialise(): void {
+    $("#addClick").on("click", () => {
+        addBothNums()
+    })
+}
+
+function addBothNums(numOne: number, numTwo: number)
+$("#addClick").on("click"), () => {
+    return addBothNums()
+}
+
+$(function () {
+    initialise()
+})
