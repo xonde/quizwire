bb2d6462ce1c1bda5c87ef16bded7cd80bec4f1f
James Reilly <james.reilly.780@gmail.com>
2021-06-21 16:20:34 +0100

    Created quiz page
    created quiz page with current question. also started the sumbit button for the question answers

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -18,30 +18,25 @@ const dataHandler = (data: OpenTriviaResponse) => {
     const Questions = data.results.map(result => result.question)
     const Answers = data.results.map(result => result.correct_answer)
     const incorrectAnswers = data.results.map(result => result.incorrect_answers)
-    let myText: string = ""
+    let myText: string []=[]
     let allAnswers: string[]=[]
     let i: number= 0
     let QuestionNo: number= Questions.length
-    while(i < QuestionNo){ 
-        allAnswers.push(Answers[i] + "<br/>" + incorrectAnswers[i].join("<br/>"))
-        i++
-    }
     i = 0
     while(i < QuestionNo){
-        myText += `<h1>Question: <br/> ${Questions[i]}</h1>` + `<h2>Possible Answers:  <br/> ${allAnswers[i]} </h2>`
+        myText.push(`<h1>Question: <br/> ${Questions[i]}</h1>` + `<h2>Possible Answers:  <br/> ${allAnswers[i]} </h2>`)
+        allAnswers.push(Answers[i] + "<br/>" + incorrectAnswers[i].join("<br/>"))      
         i++
         // myText += "Question: <br/>" + value + "<br/>"
         // Answers.forEach(function (value) {
         //     myText += "Answer: <br/>" + value + "<br/>"
         // })
     };
-    $("#questions").html(myText)
-    
+    i=0
+    while (i < QuestionNo){
+        $("#currentQues").html(Questions[i])
+        i++
+    };
 }
 
-
 $.get("https://opentdb.com/api.php?amount=10", dataHandler)
-
-$(() => {
-
-})
